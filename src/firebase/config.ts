8b9478e6d6b604a52f5c1fc59fbb05add850d7c2
Sergey Sudakov <gofavec312@jalunaki.com>
2024-06-09 16:39:02 +0700

    some minor changes

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeaag9oqU6VfHvtAEsiEBJbE_IYYgGJXU",
+  authDomain: "furniro-69e84.firebaseapp.com",
+  projectId: "furniro-69e84",
+  storageBucket: "furniro-69e84.appspot.com",
+  messagingSenderId: "591367852311",
+  appId: "1:591367852311:web:9c1088bcd0fdc7141ce752",
+  measurementId: "G-0EEY8S1ZDT",
+}
+
+const app = initializeApp(firebaseConfig)
+export const analytics = getAnalytics(app)
+export const db = getFirestore(app)
