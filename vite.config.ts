b97309958425591249ab498badeefa47f6554946
Sergey Sudakov <kkotovic14@gmail.com>
2024-07-28 18:55:21 +0700

    update: added workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://edisss1.github.io/Furniro/",
+  base: "/Furniro/",
   build: {
     target: "esnext",
   },
