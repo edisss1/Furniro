8e97a193eada7bc9e9f8295a5c71f57fdf1833ab
Sergey Sudakov <kkotovic14@gmail.com>
2024-07-28 18:49:25 +0700

    update: changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://edisss1.github.io/Furniro/",
   build: {
     target: "esnext",
   },
