c83a5804b621254cd72b2fff588b5f57595063c2
Sergey Sudakov <kkotovic14@gmail.com>
2024-08-10 15:33:07 +0700

    fix: minor fixes

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -10,17 +10,14 @@ import Footer from "../components/utilityComponents/generalComponents/Footer"
 
 import NavBar from "../components/utilityComponents/generalComponents/NavBar"
 import Guarantees from "../components/utilityComponents/Guarantees"
-import Loading from "../components/utilityComponents/Loading"
-import { ProductContext } from "../context/ProductsContext"
+
 import { useFilter } from "../context/FilterContext"
 
 const ShopPage = () => {
   useEffect(() => {
     document.title = "Shop"
   }, [])
-  // const context = useContext(ProductContext)
-  // if (!context) return <Loading />
-  // const { products } = context
+
   const { filteredProducts } = useFilter()
   return (
     <>
