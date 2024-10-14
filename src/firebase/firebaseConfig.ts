1ccc9f00aefd55635a5b2a9eb0cb2b8478ebb801
Sergey Sudakov <kkotovic14@gmail.com>
2024-10-14 15:17:54 +0700

    load env change testing

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,14 +1,18 @@
-import { initializeApp, FirebaseOptions } from "firebase/app"
+import { initializeApp } from "firebase/app"
 
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
-import { fetchFirebaseConfig } from "./config"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
-const firebaseOptions = (await fetchFirebaseConfig()) as FirebaseOptions
-
 const firebaseConfig = {
-  ...firebaseOptions,
+  // ...firebaseOptions,
+  apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
+  authDomain: import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRESTORE_APP_ID,
+  measurementId: import.meta.env.VITE_FIRESTORE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
